cf0c481015fda03372dbc3cbf3eac6f0d7755a42
acornim <youn_306@naver.com>
2020-01-06 11:17:23 +0900

    Day 3 : inline style

---
+++ Study Notes/Day 3 : .md
@@ -0,0 +1,30 @@
+### inline style
+- curly braces 써야함. 다 wrap up 해주기 -> 안쪽 {}는 object로 만들어주었다 바깥 {}로 감싸지면 JS라는 의미
+- JS에서는 -(dash)쓰지 못함. 그래서 -대신 대문자로 쓰기
+- 예: background-color 는 backgroundColor로 바꿔줘야함
+- JSX 안에서 style 길어지면 너무 가독성 떨어짐
+- 그래서 const로 따로 변수 만들어주자! 
+
+```
+const date = new Date()
+   const hours = date.getHours()
+   let timeOfDay 
+   const styles = {
+    backgroundColor : "#32376b",
+    fontSize : 24
+}
+   if(hours<12){
+       timeOfDay = "morning"
+       styles.color = "#1abc9c"
+   } else if(hours>=12 && hours<17){
+       timeOfDay = "afternoon"
+       styles.color = "#e74c3c"
+   } else {
+       timeOfDay = "night"
+       styles.color = "#2c3e50"
+   }
+   
+    return (
+    <h1 style={styles}>it's {hours % 12} o'clock. good {timeOfDay}</h1>
+   )
+```
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,17 +1,25 @@
 import React from 'react'
 function MainContent(){
+   const date = new Date()
+   const hours = date.getHours()
+   let timeOfDay 
+   const styles = {
+    backgroundColor : "#32376b",
+    fontSize : 24
+}
+   if(hours<12){
+       timeOfDay = "morning"
+       styles.color = "#1abc9c"
+   } else if(hours>=12 && hours<17){
+       timeOfDay = "afternoon"
+       styles.color = "#e74c3c"
+   } else {
+       timeOfDay = "night"
+       styles.color = "#2c3e50"
+   }
+   
     return (
-        <div>
-            <input type='checkbox'/>
-            <p>placeholder text here</p>
-
-            <input type='checkbox'/>
-            <p>placeholder text here</p>
-            
-            <input type='checkbox'/>
-            <p>placeholder text here</p>
-           
-        </div>
-    )
+    <h1 style={styles}>it's {hours % 12} o'clock. good {timeOfDay}</h1>
+   )
 }
 export default MainContent;
